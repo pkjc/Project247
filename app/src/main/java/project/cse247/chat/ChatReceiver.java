b917d7df285bf5a81e00be0312a07d4b89e7ac30
Noah G <nagiles@oakland.edu>
2017-03-20 11:16:44 -0400

    Experimentation

--- app/src/main/java/project/cse247/chat/ChatReceiver.java
+++ app/src/main/java/project/cse247/chat/ChatReceiver.java
@@ -3,6 +3,8 @@ package project.cse247.chat;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.NetworkInfo;
+import android.net.wifi.p2p.WifiP2pConfig;
 import android.net.wifi.p2p.WifiP2pDevice;
 import android.net.wifi.p2p.WifiP2pManager;
 import android.util.Log;
@@ -18,6 +20,8 @@ import java.util.ArrayList;
  * Created by Noah on 3/6/2017.
  */
 
+//Handles intents to/from the system, such as the detection of a new peer
+//Or the changing of the connection
 public class ChatReceiver extends BroadcastReceiver {
 
     private WifiP2pManager manager;
@@ -25,7 +29,7 @@ public class ChatReceiver extends BroadcastReceiver {
     private MainActivity mainActivity;
 
     public ChatReceiver(WifiP2pManager manager, WifiP2pManager.Channel channel,
-                        MainActivity mainActivity){
+                        MainActivity mainActivity) {
         super();
         this.manager = manager;
         this.channel = channel;
@@ -36,40 +40,32 @@ public class ChatReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         String action = intent.getAction();
         Log.d("Recieve: ", action);
-        ListView peerListView = (ListView) mainActivity.findViewById(R.id.peer_list);
 
         switch (action){
             case WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION:
                 break;
             case WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION:
+
+                //A new peer has been discovered!
+                //Ask the WifiP2pManager to update the list of peers
                 manager.requestPeers(channel, mainActivity.peerListListener);
-                ArrayList<String> peerList = new ArrayList<>();
 
-                for (WifiP2pDevice wifiP2pDevice : mainActivity.peers){
-                    peerList.add(wifiP2pDevice.deviceName);
-                }
-
-                ArrayAdapter<String> peerListAdapter = new ArrayAdapter<>(mainActivity, android.R.layout.simple_list_item_1, peerList);
-
-                peerListView.setAdapter(peerListAdapter);
-
-                peerListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                    @Override
-                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                        //TODO
-                    }
-                });
-
-               /* String t = "Detected Peers: \n";
+                //Print the detected devices to the main screen
+                mainActivity.deviceList.setText("Devices: \n");
 
                 for (WifiP2pDevice p : mainActivity.peers){
-                    t += "Name: " + p.deviceName + "\tAddress: " + p.deviceAddress+"\n";
+                    mainActivity.deviceList.append(p.deviceName + "\t" + p.deviceAddress + "\n");
                 }
-
-                mainActivity.message.setText(t);*/
 
                 break;
             case WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION:
+
+                NetworkInfo networkInfo = (NetworkInfo) intent.getParcelableExtra(WifiP2pManager.EXTRA_NETWORK_INFO);
+
+                if (networkInfo.isConnected()){
+                    manager.requestConnectionInfo(channel, mainActivity.connectionInfoListener);
+                }
+
                 break;
             case WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION:
                 break;
--- app/src/main/java/project/cse247/chat/MainActivity.java
+++ app/src/main/java/project/cse247/chat/MainActivity.java
@@ -3,20 +3,30 @@ package project.cse247.chat;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.IntentFilter;
+import android.net.wifi.WpsInfo;
+import android.net.wifi.p2p.WifiP2pConfig;
 import android.net.wifi.p2p.WifiP2pDevice;
 import android.net.wifi.p2p.WifiP2pDeviceList;
+import android.net.wifi.p2p.WifiP2pInfo;
 import android.net.wifi.p2p.WifiP2pManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
+import java.net.Inet4Address;
+import java.net.InetAddress;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    public TextView message;
+    public TextView deviceList;
 
     private WifiP2pManager manager;
     private WifiP2pManager.Channel channel;
@@ -25,6 +35,9 @@ public class MainActivity extends AppCompatActivity {
 
     public List<WifiP2pDevice> peers = new ArrayList<>();
 
+    /*
+    Create a PeerListener which simply puts detected devices into a list
+     */
     public WifiP2pManager.PeerListListener peerListListener = new WifiP2pManager.PeerListListener() {
         @Override
         public void onPeersAvailable(WifiP2pDeviceList peerList) {
@@ -36,13 +49,41 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
+    public WifiP2pManager.ConnectionInfoListener connectionInfoListener = new WifiP2pManager.ConnectionInfoListener() {
+        @Override
+        public void onConnectionInfoAvailable(final WifiP2pInfo info) {
+
+            InetAddress groupOwnerAddress = info.groupOwnerAddress;
+
+            Log.d("Debugging", "Enter onConnectionInfoAvailable");
+
+            if (info.groupFormed && info.isGroupOwner){
+                Log.d("Group", "GROUP FORMED; CURRENTLY GROUP LEADER");
+            }
+            else if (info.groupFormed){
+                Log.d("Group", "GROUP FORMED");
+            }
+            else{
+                Log.d("Group", "NO GROUP");
+            }
+
+            Log.d("Group", "Group owner: " + groupOwnerAddress.toString());
+
+        }
+    };
+
+    /*
+    Do initialization stuff for the app
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        Log.d("Startup", "Startup initiated");
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        message = (TextView) this.findViewById( R.id.text_view);
-        message.setText("Detected Peers: \n");
+        deviceList = (TextView) findViewById(R.id.device_list);
 
         manager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
         channel = manager.initialize(this, getMainLooper(), null);
@@ -69,12 +110,52 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    public void do_connect(View view){
+
+        Log.d("Connection", "Attempting to connect to peers");
+
+        //TESTING
+        for (WifiP2pDevice p : peers){
+            WifiP2pConfig config = new WifiP2pConfig();
+            config.deviceAddress = p.deviceAddress;
+
+            manager.connect(channel, config, new WifiP2pManager.ActionListener() {
+                @Override
+                public void onSuccess() {
+                    Log.d("Connection", "CONNECTION ESTABLISHED!");
+                }
+
+                @Override
+                public void onFailure(int reason) {
+                    Log.d("Connection", "FAILED!");
+                }
+            });
+        }
+    }
+
+    /*
+    Handle resuming the activity
+     */
     @Override
     protected void onResume(){
         super.onResume();
         registerReceiver(receiver, filter);
+        manager.discoverPeers(channel, new WifiP2pManager.ActionListener() {
+            @Override
+            public void onSuccess() {
+
+            }
+
+            @Override
+            public void onFailure(int reason) {
+
+            }
+        });
     }
 
+    /*
+    Handle pausing the activity
+     */
     @Override
     protected void onPause(){
         super.onPause();
